adaf56b6fcf797109839a467f1899203b77e8c1a
Nicolas Blaisdell <nblaisdell2@gmail.com>
2021-10-18 19:34:23 -0400

    whatever

--- components/Header.js
+++ components/Header.js
@@ -1,4 +1,4 @@
-import Image from "next/image";
+// import Image from "next/image";
 import HeaderItem from "./HeaderItem";
 import {
   BadgeCheckIcon,
--- components/Thumbnail.js
+++ components/Thumbnail.js
@@ -1,4 +1,4 @@
-import Image from "next/image";
+// import Image from "next/image";
 import { ThumbUpIcon } from "@heroicons/react/outline";
 import { forwardRef } from "react";
 
